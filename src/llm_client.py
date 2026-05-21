daa72df3529836a158d98032bee7a361d3f9202e
ZuziR6 <gabrielgoliver1@gmail.com>
2026-05-21 08:36:05 -0300

    Update llm_client.py

--- src/llm_client.py
+++ src/llm_client.py
@@ -1,1 +1,25 @@
+import ollama
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+MODEL = os.getenv("OLLAMA_MODEL", "gpt-oss:120b")
+
+
+class LLMClient:
+    def __init__(self):
+        self.model = MODEL
+
+    def gerar(self, prompt: str):
+        resposta = ollama.chat(
+            model=self.model,
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ]
+        )
+
+        return resposta["message"]["content"]
